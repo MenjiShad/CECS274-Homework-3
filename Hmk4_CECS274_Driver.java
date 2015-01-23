33c0bdf7a6888e8e93f8d852f43355e581e5d211
James Dinh <jamesdinh14@gmail.com>
2015-01-22 17:49:16 -0800

    Create Hmk4_CECS274_Driver.java

---
+++ Hmk4_CECS274_Driver.java
@@ -0,0 +1,10 @@
+public class Hmk4_CECS274_Driver
+{
+  public static void main(String[] IDontWantToDoThisAnyMore)
+  {
+    Hmk4_CECS274 call = new Hmk4_CECS274();
+    call.info();
+    call.setLinkedListSize();
+    call.elementFinder();
+  }
+}
